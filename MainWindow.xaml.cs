fbe6e88d2ec167a7191a42925e92e88333450f80
Sherry Ummen <sherry.ummen@napa.fi>
2016-12-08 10:31:34 +0200

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RadDockingActivateUnpinnedPaneWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RadButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(this.pane1.IsActive)
+            {
+                this.pane1.IsActive = false;
+            }
+            else
+            {
+                this.pane1.IsActive = true;
+            }
+        }
+    }
+}
